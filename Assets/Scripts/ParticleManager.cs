6036defb34ee5d6d92863646d80c716bc949ccee
AngryBirman <lannistumatonn@gmail.com>
2018-12-09 17:07:50 +0200

    - Added particle effect when screen is touched (Note: only works with touchscreen not with mouse!)

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// spawns particles to location where player taps with finger
+public class ParticleManager : MonoBehaviour {
+
+    public GameObject particlesToSpawn; // our particles that we want to spawn (set from editor)
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.touchCount > 0) // dont spawn too fast
+        {
+            Touch touch = Input.GetTouch(0); // we get the touch
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position); // we get the touch position
+
+            if (touch.phase == TouchPhase.Began) // if touched we spawn with instantiate method
+                Instantiate(particlesToSpawn, touchPos, Quaternion.identity); // spawns particles to touch location
+        }
+	}
+}
