97bca8a50b2627adff36f4d9aa5d3040102e81d0
AngryBirman <lannistumatonn@gmail.com>
2018-12-17 19:38:39 +0200

    - Added snowman that can be built by tapping the right place.

---
+++ Assets/Scripts/snowballManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowballManager : MonoBehaviour
+{
+ 
+    public GameObject snowball;
+
+    private int ballCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballCount < 3) // just limits how many snowballs we can spawn per game
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+
+            // on touched
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+            {
+                if (Input.touchCount == 1)
+                {
+                    Instantiate(snowball, touchPos, Quaternion.identity); // spawns snowball to touch position
+                    ballCount++;
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/snowmanBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowmanBuilder : MonoBehaviour {
+
+    List<GameObject> snowmanParts = new List<GameObject>();
+    private int index = 0;
+
+    public GameObject ball1;
+    public GameObject ball2;
+    public GameObject ball3;
+
+    public GameObject face;
+    public GameObject hands;
+    public GameObject buttons;
+
+    private void Start()
+    {
+        snowmanParts.Add(ball1);
+        snowmanParts.Add(ball2);
+        snowmanParts.Add(ball3);
+        snowmanParts.Add(face);
+        snowmanParts.Add(hands);
+        snowmanParts.Add(buttons);
+    }
+
+    public void buildSnowman()
+    {
+        snowmanParts[index].SetActive(true);
+        index++;
+    }
+    
+}
