5e291c1e818ffdb73cdb1a1ec4046b613e3d19eb
AngryBirman <lannistumatonn@gmail.com>
2018-12-18 20:18:18 +0200

    - Updated project to Unity 2018.3.0f2 - Started working on elf and gift.

---
+++ Assets/Scripts/elfManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elfManager : MonoBehaviour {
+
+    public GameObject elf;
+
+    public GameObject elfSpawnLocation;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        Instantiate(elf, elfSpawnLocation.transform.position, Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showElf()
+    {
+        elf.SetActive(true);
+    }
+}
---
+++ Assets/elf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elf : MonoBehaviour {
+
+
+    Animator m_Animator;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        m_Animator = gameObject.GetComponent<Animator>(); // instantiating animator
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
