a57bec73381ae3e1ba4db3aab1d45cc50df42cff
truonti <teemu.ruonti@edu.turkuamk.fi>
2018-12-18 20:33:57 +0200

    Limit ornate ball spawns to 1

--- Assets/Scripts/KoristepalloManager.cs
+++ Assets/Scripts/KoristepalloManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class KoristepalloManager : MonoBehaviour
 {
     public GameObject koristepallo; // object (ornate ball) that we want to spawn
+    public bool spawned = false;
     
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,10 @@ public class KoristepalloManager : MonoBehaviour
         // on touch to tree hollow
         if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
         {
-            if (Input.touchCount == 1)
+            if (Input.touchCount == 1 && spawned == false)
             {
                 Instantiate(koristepallo, touchPos, Quaternion.identity); // spawns an ornate ball to the tree hollow at touch position
+                spawned = true;
             }
 
         }
